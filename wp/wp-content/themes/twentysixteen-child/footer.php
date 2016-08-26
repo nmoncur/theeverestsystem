a2c74112ce5e2a9e59f12073f8e77df0f38581af
everest <sdm_everest@ps531219.dreamhostps.com>
2016-08-25 18:51:17 -0700

    created style.css, footer.php, functions.php, header.php, home.php

---
+++ wp/wp-content/themes/twentysixteen-child/footer.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+?>
+
+                </div><!-- .site-content -->
+
+        </div><!-- .site-inner -->
+</div><!-- .site -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ wp/wp-content/themes/twentysixteen-child/home.php
@@ -0,0 +1,12 @@
+<?php
+  /* Template Name: Home */
+  get_header();
+?>
+<div id="primary" class="content-area">
+  <main id="main" class="site-main" role="main">
+
+    <div class="row"> asdfasdfasdf </div>
+
+  </main>
+</div>
+<?php get_footer(); ?>
