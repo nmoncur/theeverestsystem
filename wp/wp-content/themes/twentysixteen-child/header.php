068ffc6cdac13ccc059309fc9ffacd665a939a2b
Nick Moncur <nick.moncur@blinkss.com>
2016-08-29 23:23:19 -0600

    Completed initial work on theme - updated child theme template, built out the header

--- wp/wp-content/themes/twentysixteen-child/header.php
+++ wp/wp-content/themes/twentysixteen-child/header.php
@@ -9,8 +9,36 @@
         <?php endif; ?>
         <?php wp_head(); ?>
 </head>
-
 <body <?php body_class(); ?>>
-<div id="page" class="site">
-  <div class="site-inner">
+  <div id="page" class="site">
+    <div class="site-inner">
+    <header id="masthead" class="site-header" role="banner">
+      <div class="row">
+        <div class="col-sm-3 text-center">
+          <div class="main-logo">
+            <img src="<?php echo get_template_directory_uri() ?>-child/images/logo.png" alt="" />
+          </div>
+        </div>
+        <div class="col-sm-5 col-sm-offset-4">
+          <?php if ( has_nav_menu( 'primary' ) ) : ?>
+            <button id="menu-toggle" class="menu-toggle"><?php _e( 'Menu', 'twentysixteen' ); ?></button>
+
+            <div id="site-header-menu" class="site-header-menu">
+              <?php if ( has_nav_menu( 'primary' ) ) : ?>
+                <nav id="site-navigation" class="main-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Primary Menu', 'twentysixteen' ); ?>">
+                  <?php
+                    wp_nav_menu( array(
+                      'theme_location' => 'primary',
+                      'menu_class'     => 'primary-menu',
+                     ) );
+                  ?>
+                </nav><!-- .main-navigation -->
+              <?php endif; ?>
+
+            </div><!-- .site-header-menu -->
+          <?php endif; ?>
+
+        </div>
+      </div>
+    </header>
     <div id="content" class="site-content">
